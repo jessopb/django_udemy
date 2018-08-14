0a20b8425999b188be7bae009ae2c11225827901
Jessop Breth <jessopb@gmail.com>
2018-08-14 15:15:54 -0400

    User can use app url routing to get to 'index/' and 'users/'

--- django_p2/django_app/views.py
+++ django_p2/django_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # dict = {'content':'WELCOME'}
+    return HttpResponse('Hello')
+
+def users(request):
+    return HttpResponse('Users')
