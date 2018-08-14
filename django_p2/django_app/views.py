0c476a2fa024b29f55ea7f737b90220f02ea507b
Jessop Breth <jessopb@gmail.com>
2018-08-14 15:49:24 -0400

    Renders html and dynamic content from templates

--- django_p2/django_app/views.py
+++ django_p2/django_app/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    # dict = {'content':'WELCOME'}
-    return HttpResponse('Hello')
+    cdict = {'content': 'Index Stuff'}
+    return render(request, 'django_app/index.html', context=cdict)
+
 
 def users(request):
-    return HttpResponse('Users')
+    users = ['Tom', 'Bob', 'Sam']
+    udict = {'users': users}
+    return render(request, 'django_app/users.html', context=udict)
