ff0b09b98a01aac778b64dff8757bafd7f9c08c0
Jessop Breth <jessopb@gmail.com>
2018-08-23 11:49:42 -0400

    Adds validation to form.html; styling

--- django_p2/django_app/forms.py
+++ django_p2/django_app/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from django.core import validators
 from django_app.models import OurUsers
 
+
 class UserForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    first_name = forms.CharField(
+        required=True,
+        validators=[validators.MaxLengthValidator(15),
+                    validators.MinLengthValidator(2),
+                    validators.RegexValidator(r'^[A-Za-z]*$')])
+    last_name = forms.CharField(
+        required=True,
+        validators=[validators.MaxLengthValidator(15),
+                    validators.MinLengthValidator(2),
+                    validators.RegexValidator(r'^[A-Za-z]*$')])
     email = forms.EmailField()
+
 
 class UserModelForm(forms.ModelForm):
     first_name = forms.CharField()
